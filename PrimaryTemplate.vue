7f6b0954ca792552dccce980be90ebcec49b3d12
Lucy Stedman <stedm035@umn.edu>
2024-12-04 15:14:52 -0600

    Added Social Media Icons

--- PrimaryTemplate.vue
+++ PrimaryTemplate.vue
@@ -35,6 +35,42 @@
         <a href="">612-900-8101</a>
 
         <div class="media-icons">
+          <router-link :to="{ name }">
+          <svg xmlns="http://www.w3.org/2000/svg"  viewBox="0,0,256,256" width="48px"
+               height="48px" fill-rule="nonzero">
+            <g fill="#ffffff" fill-rule="nonzero" stroke="none" stroke-width="1" stroke-linecap="butt" stroke-linejoin="miter"
+               stroke-miterlimit="10" stroke-dasharray="" stroke-dashoffset="0" font-family="none"
+               font-size="none" style="mix-blend-mode: normal">
+              <g transform="scale(10.66667,10.66667)">
+                <path d="M11.666,2.005c-5.046,0.165 -9.292,4.246 -9.641,9.283c-0.369,5.329 3.442,9.832 8.481,10.589v-7.227h-1.614c-0.726,0 -1.314,-0.588 -1.314,-1.314v0c0,-0.726 0.588,-1.314 1.314,-1.314h1.613v-1.749c0,-2.896 1.411,-4.167 3.818,-4.167c0.357,0 0.662,0.008 0.921,0.021c0.636,0.031 1.129,0.561 1.129,1.198v0c0,0.663 -0.537,1.2 -1.2,1.2h-0.442c-1.022,0 -1.379,0.969 -1.379,2.061v1.437h1.87c0.591,0 1.043,0.527 0.953,1.111l-0.108,0.701c-0.073,0.47 -0.477,0.817 -0.953,0.817h-1.762v7.247c4.883,-0.663 8.648,-4.837 8.648,-9.899c0,-5.634 -4.659,-10.179 -10.334,-9.995z"></path>
+              </g>
+            </g>
+          </svg>
+          </router-link>
+          <router-link :to="{ name }">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0,0,256,256" width="48px"
+               height="48px" fill-rule="nonzero">
+            <g fill="#ffffff" fill-rule="nonzero" stroke="none" stroke-width="1" stroke-linecap="butt" stroke-linejoin="miter"
+               stroke-miterlimit="10" stroke-dasharray="" stroke-dashoffset="0" font-family="none"
+               font-size="none" style="mix-blend-mode: normal">
+              <g transform="scale(10.66667,10.66667)">
+                <path d="M8,3c-2.761,0 -5,2.239 -5,5v8c0,2.761 2.239,5 5,5h8c2.761,0 5,-2.239 5,-5v-8c0,-2.761 -2.239,-5 -5,-5zM18,5c0.552,0 1,0.448 1,1c0,0.552 -0.448,1 -1,1c-0.552,0 -1,-0.448 -1,-1c0,-0.552 0.448,-1 1,-1zM12,7c2.761,0 5,2.239 5,5c0,2.761 -2.239,5 -5,5c-2.761,0 -5,-2.239 -5,-5c0,-2.761 2.239,-5 5,-5zM12,9c-1.65685,0 -3,1.34315 -3,3c0,1.65685 1.34315,3 3,3c1.65685,0 3,-1.34315 3,-3c0,-1.65685 -1.34315,-3 -3,-3z"></path>
+              </g>
+            </g>
+          </svg>
+          </router-link>
+          <router-link :to="{ name }">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0,0,256,256" width="48px"
+               height="48px" fill-rule="nonzero">
+            <g fill="#ffffff" fill-rule="nonzero" stroke="none" stroke-width="1" stroke-linecap="butt" stroke-linejoin="miter"
+               stroke-miterlimit="10" stroke-dasharray="" stroke-dashoffset="0" font-family="none"
+               font-size="none" style="mix-blend-mode: normal">
+              <g transform="scale(10.66667,10.66667)">
+                <path d="M19,3h-14c-1.105,0 -2,0.895 -2,2v14c0,1.105 0.895,2 2,2h14c1.105,0 2,-0.895 2,-2v-14c0,-1.105 -0.895,-2 -2,-2zM7.738,17v0c-0.697,0 -1.262,-0.565 -1.262,-1.262v-4.477c0.001,-0.696 0.566,-1.261 1.262,-1.261v0c0.697,0 1.262,0.565 1.262,1.262v4.477c0,0.696 -0.565,1.261 -1.262,1.261zM7.694,8.717c-0.771,0 -1.286,-0.514 -1.286,-1.2c0,-0.686 0.514,-1.2 1.371,-1.2c0.771,0 1.286,0.514 1.286,1.2c0,0.686 -0.514,1.2 -1.371,1.2zM16.779,17v0c-0.674,0 -1.221,-0.547 -1.221,-1.221v-2.605c0,-1.058 -0.651,-1.174 -0.895,-1.174c-0.244,0 -1.058,0.035 -1.058,1.174v2.605c0,0.674 -0.547,1.221 -1.221,1.221h-0.081c-0.674,0 -1.221,-0.547 -1.221,-1.221v-4.517c0,-0.697 0.565,-1.262 1.262,-1.262v0c0.697,0 1.262,0.565 1.262,1.262c0,0 0.282,-1.262 2.198,-1.262c1.219,0 2.196,0.977 2.196,3.174v2.605c0,0.674 -0.547,1.221 -1.221,1.221z"></path>
+              </g>
+            </g>
+          </svg>
+          </router-link>
         </div>
 
       </div>
@@ -151,6 +187,10 @@ section.primary-template {
     @media screen and (width >= 600px) {
       text-align: right;
     }
+  }
+
+  div.media-icons{
+    margin-top: 1rem;
   }
 
   section.main {
